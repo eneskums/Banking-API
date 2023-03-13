d56fd99f598ca6158c957d0e5840505ed15f50b4
Enes KUMAŞ <75564430+eneskums@users.noreply.github.com>
2023-03-13 16:32:39 +0300

    add IsEmployee permission and update views

--- bankingApi/app/views.py
+++ bankingApi/app/views.py
@@ -3,40 +3,42 @@ from decimal import Decimal
 from django.db import transaction
 from django.db.models import Q
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
-from .models import Customer, Account, Transfer, Log
+from .models import Customer, Account, Transfer
+from .permissions import IsEmployee
 from .serializers import AccountSerializer, TransferSerializer, CustomerSerializer
 
 
 @api_view(['POST'])
+@permission_classes([IsEmployee])
 def create_customer(request):
     customer_name = request.data.get('name')
 
-    customer = Customer(name=customer_name)
-    customer.save()
+    customer = Customer.objects.create(name=customer_name)
     serializer = CustomerSerializer(customer)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
+@permission_classes([IsEmployee])
 def create_account(request):
     customer_id = request.data.get('customer_id')
     initial_balance = request.data.get('initial_balance')
 
     try:
         customer = Customer.objects.get(pk=customer_id)
-        account = Account(customer=customer, balance=initial_balance)
-        account.save()
+        account = Account.objects.create(customer=customer, balance=initial_balance)
         serializer = AccountSerializer(account)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     except Customer.DoesNotExist:
         return Response({'error': 'Customer with id {} does not exist'.format(customer_id)},
                         status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
+@permission_classes([IsEmployee])
 def transfer(request):
     sender_account_id = request.data.get('sender_account_id')
     receiver_account_id = request.data.get('receiver_account_id')
@@ -48,7 +50,8 @@ def transfer(request):
             receiver_account = Account.objects.get(pk=receiver_account_id)
 
             if sender_account_id == receiver_account_id:
-                return Response({'error': 'Accounts are not same'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'error': 'Sender and receiver account cannot be the same'},
+                                status=status.HTTP_400_BAD_REQUEST)
 
             if transfer_amount <= 0:
                 return Response({'error': 'Invalid amount'}, status=status.HTTP_400_BAD_REQUEST)
@@ -56,9 +59,8 @@ def transfer(request):
             if sender_account.balance < transfer_amount:
                 return Response({'error': 'Insufficient balance in sender account'}, status=status.HTTP_400_BAD_REQUEST)
 
-            transfer = Transfer(source_account=sender_account, destination_account=receiver_account,
-                                amount=transfer_amount)
-            transfer.save()
+            transfer = Transfer.objects.create(sender_account=sender_account, receiver_account=receiver_account,
+                                               amount=transfer_amount)
 
             sender_account.balance -= transfer_amount
             sender_account.save()
@@ -66,20 +68,15 @@ def transfer(request):
             receiver_account.balance += transfer_amount
             receiver_account.save()
 
-            # log = Log()
-            # log.save()
-
             transfer_serializer = TransferSerializer(transfer)
-            sender_serializer = AccountSerializer(sender_account)
-            receiver_serializer = AccountSerializer(receiver_account)
 
-            return Response({'transfer': transfer_serializer.data, 'sender': sender_serializer.data,
-                             'receiver': receiver_serializer.data})
+            return Response(transfer_serializer.data, status=status.HTTP_201_CREATED)
     except Account.DoesNotExist:
         return Response({'error': 'Account with given id does not exist'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
+@permission_classes([IsEmployee])
 def account_balance(request, account_id):
     try:
         account = Account.objects.get(pk=account_id)
@@ -89,12 +86,13 @@ def account_balance(request, account_id):
 
 
 @api_view(['GET'])
+@permission_classes([IsEmployee])
 def transfer_history(request, account_id):
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
     try:
         account = Account.objects.get(pk=account_id)
-        query = Q(source_account=account) | Q(destination_account=account)
+        query = Q(sender_account=account) | Q(receiver_account=account)
         if start_date:
             query &= Q(timestamp__gte=start_date)
         if end_date:
