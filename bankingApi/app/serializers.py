7da6c1182f87f566ec0c83fa2310d230312f0657
Enes KUMAŞ <75564430+eneskums@users.noreply.github.com>
2023-03-13 16:31:23 +0300

    update serializers

--- bankingApi/app/serializers.py
+++ bankingApi/app/serializers.py
@@ -1,12 +1,14 @@
+from decimal import Decimal
+
 from rest_framework import serializers
 
-from .models import Employee, Customer, Account, Transfer, Log
+from .models import Employee, Customer, Account, Transfer
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ('id', 'name', 'email', 'password')
+        fields = ('id', 'first_name', 'last_name', 'username', 'password')
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -21,22 +23,18 @@ class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = '__all__'
-        depth = 3
 
 
 class TransferSerializer(serializers.ModelSerializer):
     source_account_customer_name = serializers.ReadOnlyField(source='source_account.customer.name')
     destination_account_customer_name = serializers.ReadOnlyField(source='destination_account.customer.name')
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         model = Transfer
         fields = '__all__'
-        depth = 3
 
-
-class LogSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(source='employee.user.username', read_only=True)
-
-    class Meta:
-        model = Log
-        fields = ['user', 'timestamp', 'from_account', 'to_account', 'amount']
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['amount'] = round(Decimal(ret['amount']), 2)
+        return ret
